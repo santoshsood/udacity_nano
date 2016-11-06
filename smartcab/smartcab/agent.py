00f6b2eb14f63a1a07ea9dd1b4e9315b5fa87ee8
santoshsood <santosh.sood@gmail.com>
2016-11-05 23:22:37 -0700

    updated smartcab 11/05

--- smartcab/smartcab/agent.py
+++ smartcab/smartcab/agent.py
@@ -2,6 +2,9 @@ import random
 from environment import Agent, Environment
 from planner import RoutePlanner
 from simulator import Simulator
+from agent1 import QLearningAgent
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 class LearningAgent(Agent):
@@ -14,96 +17,104 @@ class LearningAgent(Agent):
         self.state = None
         self.next_waypoint = None
         # TODO: Initialize any additional variables here
+        self.epsilon=0.99
         self.Qdict= {}
-        self.epsilon=0.1
-        self.alpha=0.8      #learning rate
+        self.alpha=0.5      #learning rate
         self.discount=0.3   #discount future rewards
         self.state=None
         self.previous_state=None
         self.reward=None
-        self.previous_reward=0
+        self.previous_reward=None
         self.sum_rewards=0
         self.deadline=self.env.get_deadline(self)
-        self.temp = 0.92
         self.valid=0
         self.violations=0
         self.deadline_vio=0
+        self.previous_action=None
+        self.temp=0.99
+        self.deadlock_acc=0
+        self.deadlock=0
+        self.count=0;
+        self.deadline_end=0;
+        self.traffic_vio=[]
+        self.dict_len=[]
+        self.time_vio=[]
+        self.time_left=[]
 
-
-        
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
         # TODO: Prepare for a new trip; reset any variables here, if required
-        #print self.Qdict
+        self.traffic_vio.append(self.violations)
+        self.dict_len.append(len(self.Qdict))
+        self.time_vio.append(self.deadline_vio)
+        self.time_left.append(self.deadline_end)
+        print self.time_left
+        print "count :{} |traffic : {} | deadline  : {} deadlock: {} " .format(self.count, self.violations , self.deadline_vio,self.deadlock_acc)
+        print "count :{} | deadlock  :{}" .format(self.count,self.deadlock)
         print self.epsilon
-        print "traffic violations : {} | deadline violations : {} " .format(self.violations , self.deadline_vio)
+        print len(self.Qdict)
         self.state = None
-        self.next_waypoint = None
+        self.deadlock=0
         self.previous_state=None
-        self.reward=None
+        self.previous_action=None
         self.previous_reward=0
         self.sum_rewards=0
         self.epsilon=self.epsilon*self.temp
-        
-        
+        self.count+=1
 
     def stochastic(self,p):
-    #epsilon function 
         r=random.random()
         return r<p 
-
-    def Qdict_update(self,state,action,reward,next_state):
-    #initializes Q value for all state,action pairs    
-
-        if ((state,action) not in self.Qdict):
-            #print "state,action not in Qvalues \n", state ,action
-            self.Qdict[(state,action)]=20
-        else:
-            cur_value=self.Qdict[(state,action)]
-            new_value=reward + self.discount * self.Qdict[(next_state,action)]
-            learned_value=(1-self.alpha)*cur_value + self.alpha*new_value
-            self.Qdict[(self.state,action)]=learned_value
-            #print "cur_val", cur_value, "new_valu",new_value , "learned_value" ,learned_value
-
-
-    def create_state(self):
-    # create state based on all parameters of agent
-    # can be simplified further based on knowledge of traffic rules 
-        inputs = self.env.sense(self)
-        #print inputs
-        return "{}|{}|{}|{}".format(self.planner.next_waypoint(), inputs["light"], inputs["oncoming"], inputs["left"])
-        #return "{}|{}".format(inputs["light"], self.planner.next_waypoint())
-        #return "{}".format(inputs["light"])
-        #state = {"light":inputs["light"],"oncoming":inputs["oncoming"],"left":inputs["left"],"right":inputs["right"]}    
-        #return state 
+    
+    def legal_actions(self):
+        return ['forward', 'left', 'right', None]
 
     def get_QValue(self, state, action):
-        return self.Qdict.get((state, action),20)  ##return the value from the qDict, default to 0 if the key isnt in the dict
-    
+        return self.Qdict.get((state, action),20)  
+
+    def get_value_argmax(self,state): 
+    # given state best action possible
+        bestQValue = - 9999
+        for action in self.legal_actions():
+            if(self.get_QValue(state, action) > bestQValue):
+                bestQValue = self.get_QValue(state, action)
+
+        return bestQValue
+
     def policy(self):
     # Return best action from current state based on Q_value
-        self.state=self.create_state()
-        legal_actions=['forward', 'left', 'right']
-
         Best_Qval=-99999;
         best_action=None;
-        for actions in legal_actions:
-            #print "#2", self.get_QValue(self.state,actions) 
-            if(self.get_QValue(self.state,actions) > Best_Qval):
-                Best_Qval=self.get_QValue(self.state,actions)
-                best_action=actions;
-            if((self.get_QValue(self.state,actions) == Best_Qval) & self.stochastic(self.epsilon)):
-                Best_Qval=self.get_QValue(self.state,actions)
-                best_action=actions;
+        for action in self.legal_actions():
+            if(self.get_QValue(self.state, action) > Best_Qval):
+                Best_Qval = self.get_QValue(self.state, action)
+                best_action=action
+            if(self.get_QValue(self.state, action) == Best_Qval):
+                if (self.stochastic(0.5)):
+                    Best_Qval = self.get_QValue(self.state, action)
+                    best_action=action
+                
+        return best_action    
 
-        return best_action           
+
+    def Qdict_update(self,previous_state,previous_action,previous_reward,next_state):
+        #print ( previous_state, previous_action,reward,next_state, "\n")
+        if ((previous_state,previous_action) not in self.Qdict):
+            self.Qdict[(previous_state,previous_action)]=20
+        else:
+            self.Qdict[(previous_state,previous_action)]=self.Qdict[(previous_state,previous_action)]+self.alpha*(
+            previous_reward + self.discount*self.get_value_argmax(next_state)- self.Qdict[(previous_state,previous_action)])
+     
+    def create_state(self):
+        inputs = self.env.sense(self)
+        return "{}|{}|{}|{}".format(self.planner.next_waypoint(), inputs["light"], inputs["oncoming"], inputs["left"])
+     
 
 
     def update(self, t):
         # Gather inputs
         
-        valid_actions = ['forward', 'left', 'right', None]
         self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
         inputs = self.env.sense(self)
         deadline = self.env.get_deadline(self)
@@ -116,7 +127,7 @@ class LearningAgent(Agent):
         # TODO: Select action according to your policy
         if(self.stochastic(self.epsilon)):
           #print "random action"  
-          action=random.choice(valid_actions[1:])
+          action=random.choice(self.legal_actions())
         else:
           #print "policy action"   
           action=self.policy()
@@ -126,20 +137,26 @@ class LearningAgent(Agent):
         
         if (reward == -1.0) :
            self.violations += 1
+           reward=20*reward
            #print self.state , action
 
         if (deadline == 0) :
             self.deadline_vio += 1           
         
+        if (action == None) & (reward != 0):
+            self.deadlock+=1;
+            self.deadlock_acc+=1;
+
+
+        if self.previous_reward!= None:
+          self.Qdict_update(self.previous_state,self.previous_action,self.previous_reward,self.state)
 
         self.previous_reward = reward
         self.sum_rewards += reward
         self.previous_state = self.state
+        self.previous_action=action
+        self.deadline_end=self.env.get_deadline(self)    
 
-        self.Qdict_update(self.previous_state,action,reward,self.state)
-
-            
-        
         """
         # Dumb agent with random selection of actions
         action = None
@@ -154,12 +171,17 @@ class LearningAgent(Agent):
 
         #print "next_waypoint: {} action:{} reward:{} ".format(self.next_waypoint,action,reward)  # [debug]
         #print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {} violations = {}".format(deadline, inputs, action, reward self.violations)  # [debug]
+
 def run():
     """Run the agent for a finite number of trials."""
 
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
+    
+
     a = e.create_agent(LearningAgent)  # create agent
+    
+
     e.set_primary_agent(a, enforce_deadline=True)  # specify agent to track
     # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
 
@@ -169,7 +191,22 @@ def run():
     
     sim.run(n_trials=1000)  # run for a specified number of trials
     # NOTE: To quit midway, press Esc or close pygame window, or hit Ctrl+C on the command-line
-
+    plt.figure(1)
+    plt.subplot(411)
+    plt.plot(a.traffic_vio,'r--')
+    plt.ylabel("traffic violatoins")
+    plt.subplot(412)
+    plt.plot(a.dict_len,'bs')
+    plt.ylabel("states #")
+    plt.subplot(413)
+    plt.plot(a.time_vio,'bs')
+    plt.ylabel("dealine violations #")
+    plt.subplot(414)
+    plt.plot(a.time_left,'bs')
+    plt.ylabel("time left #")
+ 
+    #plt.plot(a.dict_len)
+    plt.show()
 
 if __name__ == '__main__':
     run()
